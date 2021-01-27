093b253e78b9a8c0faeb73ed5dcced52d97619d6
Cod3rBlack <cod3r.black@gmail.com>
2021-01-28 01:33:17 +0530

    first

---
+++ src/components/imgSearch.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const Search = ({ seText }) => {
+  const [text, setText] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    seText(text);
+  };
+
+  return (
+    <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
+      <form className="w-full max-w-sm" onSubmit={onSubmit}>
+        <div className="flex items-center border-b-2 border-green-500 py-2">
+          <input
+            onChange={(e) => setText(e.target.value)}
+            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            type="text"
+            placeholder="Search Image Term.."
+          />
+          <button
+            className="flex-shrink-0 bg-green-500 hover:bg-green-700 border-green-500 hover:border-green-700 text-sm border-4 text-white py-1 px-2 rounded"
+            type="submit"
+          >
+            Search
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Search;
